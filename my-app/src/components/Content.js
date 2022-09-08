c0fd778911c0912e520137b5a9208e4af91ec343
AnnieNeilson <bnefnee@gmail.com>
2022-09-08 11:38:01 +0000

    Rendering Lists Challenge completed

---
+++ my-app/src/components/Content.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import {savedPosts} from "../posts.json";
+import css from "./css/Content.module.css";
+import PostItem from './PostItems';
+
+
+
+class Content extends Component {
+    constructor(props) {
+      super(props)
+
+    }
+  render() {
+    return (
+      <div className={css.Content}>
+        <div className={css.TitleBar}>
+            <h1>My Photos</h1>
+        </div>
+        <div className={css.SearchResults}>
+            
+            <PostItem savedPosts={savedPosts} />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Content
