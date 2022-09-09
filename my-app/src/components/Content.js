15ca4025079f7c579caa3d52b984554a51f3df9d
AnnieNeilson <bnefnee@gmail.com>
2022-09-09 11:52:58 +0000

    Search Feature Challenge completed

--- my-app/src/components/Content.js
+++ my-app/src/components/Content.js
@@ -8,15 +8,28 @@ class Content extends Component {
     constructor(props) {
       super(props)
       this.state = {
-        isLoaded: false
+        isLoaded: false,
+        posts: []
      }
     }
     componentDidMount() {
         setTimeout(()=>{
             this.setState({
                 isLoaded: true,
+                posts: savedPosts,
             })
         }, 2000)
+    }
+
+    handleSearch = (event) => {
+      const inputText = event.target.value.toLowerCase()
+        console.log(inputText)
+      const filteredPosts = savedPosts.filter(post => {
+            return post.name.toLowerCase().includes(inputText)
+        })
+        this.setState({
+            posts: filteredPosts
+        })
     }
 
   render() {
@@ -25,10 +38,18 @@ class Content extends Component {
 
         <div className={css.TitleBar}>
             <h1>My Photos</h1>
+            <form>
+              <label htmlFor='searchInput'>Search:</label>
+              <input type="search" 
+              id="searchInput" 
+              placeholder="By Author"
+              onChange={ (event) => this.handleSearch(event)}></input>
+              <h4>Posts found: {this.state.posts.length}</h4>
+            </form>
         </div>
         <div className={css.SearchResults}>
             {this.state.isLoaded ?
-            <PostItem savedPosts={savedPosts} /> :
+            <PostItem savedPosts={this.state.posts} /> :
             <Loader />}
         </div>
       </div>
