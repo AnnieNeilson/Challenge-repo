c893e00dbbf8724c5ba450ecc79dd04f9b70ac13
AnnieNeilson <bnefnee@gmail.com>
2022-09-12 10:24:16 +0000

    Hooks Challenge completed

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,6 +3,7 @@ import NavBarSimple from './components/NavBarSimple';
 import Sidebar from './components/Sidebar';
 import NavBarForm from './components/NavBarForm';
 import Content from './components/Content';
+import ContentHooks from './components/ContentHooks';
 
 function App() {
   return (
@@ -10,7 +11,7 @@ function App() {
       <NavBarForm />
       {/* <NavBarSimple /> */}
       <Sidebar />
-      <Content />
+      <ContentHooks />
     </div>
   );
 }
